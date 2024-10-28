e722fdc58e266e035dcdf02508b72ab18b3f38b1
fancn21th <fancn21th@gmail.com>
2024-10-28 13:57:59 +0800

    📦 NEW: hello threejs

---
+++ script.js
@@ -0,0 +1,42 @@
+import * as THREE from "three";
+
+// canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// scene
+const scene = new THREE.Scene();
+
+// objects
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+// combine geometry and material
+const mesh = new THREE.Mesh(geometry, material);
+// add mesh to scene
+scene.add(mesh);
+
+// camera
+
+//  size config
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+camera.position.z = 3;
+
+scene.add(camera);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+// by default the camera is inside the cube, so we need to move it back as line 27
+renderer.render(scene, camera);
