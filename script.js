e065a98cc9402082af57d43a4d8babbcc9732ce7
fancn21th <fancn21th@gmail.com>
2024-10-28 17:03:47 +0800

    📦 NEW: scale, axehelper, distance

--- script.js
+++ script.js
@@ -10,12 +10,26 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
-  wireframe: true,
+  // wireframe: true,
 });
 // combine geometry and material
 const mesh = new THREE.Mesh(geometry, material);
+
+// mesh.position.x = -0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = -1;
+
+mesh.position.set(0.7, -0.6, -1);
+
+mesh.scale.set(2, 0.5, 0.5);
+
+console.log(mesh.position.length());
 // add mesh to scene
 scene.add(mesh);
+
+// axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // camera
 
@@ -27,9 +41,13 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 
-camera.position.z = 3;
+// camera.position.z = 3;
+
+camera.position.set(1, 1, 3);
 
 scene.add(camera);
+
+console.log(mesh.position.distanceTo(camera.position));
 
 // renderer
 const renderer = new THREE.WebGLRenderer({
